02901099ad3fb9c9569f59b18bd974a63fc00131
Konstantin Aksenov <axekoscode@gmail.com>
2020-03-06 00:00:42 +0300

    change in file price.py

--- lesson1/price.py
+++ lesson1/price.py
@@ -3,29 +3,47 @@ discount = 5
 
 price_with_discount = price - price * discount / 100
 
-print(price_with_discount)
+print(f'цена: {price} rub, скидка: {discount} %. \nИтоговая сумма: {price_with_discount} rub')
+print()
 
 def discount_price(price, disc):
     return price - price * disc / 100
 
-print(discount_price(100, 25))
+print('*проверка вызова функции с аргументами 100 и 25*', discount_price(100, 25))
+print()
 
+print('Сейчас мы рассчитаем итоговую цену товара на основе Ваших данных.\n')
 while True:
-    a = int(input ('Какая цена товара? '))
-    if a <=0:
-        print('Цена не может быть меньше 0!')
+    try:
+        a = int(input ('Какая цена товара? '))
+    except TypeError:
+        print('Пожалуйста, введите цену цифрами!\n')
+        continue
+    except ValueError:
+        print('Пустое значение недопустимо!\n')
+        continue
+    if a <= 0:
+        print('Извините, но мы не раздаём товары бесплатно!\n')
         continue
     break
 
-i = 3
+i = 2
 while i > 0:
-    b = int(input ('Какую скидку желаете в % ? '))
-    if b < 0 or int(b) > 100:
-        print('Скидка некорректна! Дадим 0%')
+    try:
+        b = abs(int(input ('Какую скидку желаете в % ? ')))
+    except TypeError:
+        print('Пожалуйста, введите скидку цифрами!\n')
+        i -= 1
+        continue
+    except ValueError:
+        print('Пустое значение недопустимо!\n')
+        continue
+    if b >= 100:
+        print('Значение некорректно!\n')
         b = 0
         i -= 1
         continue
     break
 
 result = discount_price(a, b)
-print('Итоговая цена для Вас:', result, 'rub')
+print(f'\nИтоговая цена для Вас: [ {result} ] rub')
